2d2d25282d587932110db128699df5268a68a5f2
Viktor <jarlaks52@gmail.com>
2019-03-22 15:20:08 +0900

    fixes

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,9 +5,8 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        films_filtered = array.reject { |film| film['country'].nil? }
+        films_filtered = array.reject { |film| film['country'].nil? || film['rating_kinopoisk'].nil? }
                               .select { |film| film['country'].split(',').size >= 2 }
-                              .reject { |film| film['rating_kinopoisk'].nil? }
                               .select { |film| film['rating_kinopoisk'].to_i.positive? }
         kinopoisk_ratings = films_filtered.map { |film| film['rating_kinopoisk'].to_f }
         kinopoisk_rating_sum = kinopoisk_ratings.reduce(:+)
@@ -16,10 +15,11 @@ module Exercise
       end
 
       def chars_count(films, threshold)
+        char = 'и'
         films_filtered = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
         films_names = films_filtered.map { |film| film['name'] }
         letters = films_names.map { |name| name.split('') }.flatten
-        letters.select { |l| l == 'и' }.size
+        letters.select { |l| l == char }.size
       end
     end
   end
