83fc179d3a1ded2c40baa6f6eacaca9a6b4880c6
Viktor <jarlaks52@gmail.com>
2019-03-26 13:00:23 +0900

    complete task rackup

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,25 @@
 module Inatra
+  @routes = {}
   class << self
-    def routes
+    def routes(&block)
+      instance_eval(&block)
     end
 
     def call(env)
+      path = env['PATH_INFO']
+      @routes[path].call
+    end
+
+    def method_missing(method_name, *arguments, &block)
+      if method_name == :get
+        @routes.store(arguments.first, block)
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      method_name == :get || super
     end
   end
 end
